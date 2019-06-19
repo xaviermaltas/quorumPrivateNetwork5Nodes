e410b1c22d4f97ca0f7e9e610f7711d6266dfdcd
Xavier Maltas <xavi.maltass@gmail.com>
2019-06-19 12:14:24 +0200

    quorum and deploy changes

--- the-election/migrations/2_deploy_contracts.js
+++ the-election/migrations/2_deploy_contracts.js
@@ -7,17 +7,19 @@ module.exports = function(deployer) {
     // .pub node 1 NqObl69ln61fiwyIi1ZTBs0mgqt6IeeRBvsWcb9AclQ=
     //.pub node2 DgQMkqB5/PP1oknXGSYvug6xN7PghfFg55P9s337BHg=
 
-    deployer.deploy(Election, { privateFor:["NqObl69ln61fiwyIi1ZTBs0mgqt6IeeRBvsWcb9AclQ="]} ).then(
-        function(data){
-            console.log("Deploying Election Contract with privateFor");
-            // console.log(data);
-        }
-    ).catch(
-        function(e){
-            console.log(e);
-            deployFunction(deployer);
-        }
-    );
+    // deployer.deploy(Election, { privateFor:["NqObl69ln61fiwyIi1ZTBs0mgqt6IeeRBvsWcb9AclQ="]} ).then(
+    //     function(data){
+    //         console.log("Deploying Election Contract with privateFor");
+    //         // console.log(data);
+    //     }
+    // ).catch(
+    //     function(e){
+    //         console.log(e);
+    //         deployFunction(deployer);
+    //     }
+    // );
+
+    deployFunction(deployer);
 };
 
 
